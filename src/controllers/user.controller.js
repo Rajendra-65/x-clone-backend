7b7a63dacf85b254f4bad23b57aa6ede659e82b7
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-08-12 13:35:00 +0530

    Developed the all the necessary routes and controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/user.model";
-import { getAuth } from "@clerk/express";
+import { clerkClient, getAuth } from "@clerk/express";
+import Notification from "../models/notification.model";
 
 export const getuserProfile = asyncHandler(async(req,res) => {
     const {username} = req.params;
@@ -35,4 +36,113 @@ export const updateProfile = asyncHandler(async(req,res) => {
     res.status(200).json({
         user
     })
+})
+
+export const syncUser = asyncHandler( async (req,res) => {
+    const { userId } = getAuth(req);
+
+    const existingUser = await User.findOne({
+        clerkId : userId
+    })
+
+    if(existingUser) {
+        return res.status(200).json({
+            user : existingUser,
+            message : "User already exists"
+        })
+    }
+
+    const clerkUser = await clerkClient.users.getUser(userId);
+
+    const userData = {
+        clerkId : userId,
+        email : clerkUser.emailAddresses[0].emailAddress,
+        firstName : clerkUser.firstName || "",
+        lastName : clerkUser.lastName || "",
+        userName : clerkUser.emailAddresses[0].emailAddress.split("@")[0],
+        profilePicture : clerkUser.imageUrl || ""
+    }
+
+    const user = await User.create(userData);
+
+    res.status(201).json({
+        user ,
+        message : "User created Successfully"
+    })
+})
+
+export const getCurrentUser = asyncHandler(async (req,res) => {
+    const {userId} = getAuth(req);
+
+    const user = await User.findOne({
+        clerkid : userId
+    })
+
+    if(!user) return res.status(404).json({
+        error : "User not found"
+    })
+
+    res.status(200).json({
+        user
+    })
+})
+
+export const followUser = asyncHandler(async (req,res) => {
+    const {userId} = getAuth(req);
+    const {targetUserId} = req.params;
+
+    if(userId === targetUserId) return res.status(400).json({
+        error : "You cannot follow yourself"
+    })
+
+    const currentUser = await User.findOne({
+        clerkId : userId
+    })
+
+    const targetUser = await User.findById(targetUserId);
+
+    if(!currentUser || !targetuser) return res.status(404).json({
+        error : "User not found"
+    })
+
+    const isFollowing = currentUser.following.includes(targetUserId);
+
+    if(isFollowing) {
+        // unFollow
+        await User.findByIdAndUpdate(currentUser._id,{
+            $pull : {
+                following : targetUserId
+            }
+        })
+        await User.findByIdAndUpdate(targetUserId,{
+            $pull : {
+                followers : currentUser._id
+            }
+        })
+    } else {
+        // Follow
+        await User.findByIdAndUpdate(
+            currentUser._id,{
+                $push : {
+                    following : targetUserId
+                }
+            }
+        )
+
+        await User.findByIdAndUpdate(targetUserId,{
+            $push : {
+                followers : currentUser._id
+            }
+        })
+
+        await Notification.create({
+            from : currentUser._id,
+            to : targetUserId,
+            type : "follow"
+        })
+
+        res.status(200).json({
+            message : isFollowing ? "User unfollowed Successfully" : "User followed Successfully"
+        })
+    }
 })
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,8 +4,14 @@ import { protectedRoute } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
+// public route
 router.get("/profile/:username", getuserProfile);
+
+// protected routes
+router.post("/sync", protectedRoute , syncUser)
+router.post("/me",protectedRoute,getCurrentUser)
 router.put("/profile",protectedRoute, updateProfile);
+router.post("/follow/:targetUserId",protectedRoute, followUser)
 
 // update profile => auth
 
