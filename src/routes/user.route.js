a34f0cec8ef5b0b85231818f8284f13d175318be
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-08-12 12:59:42 +0530

    Added the route and protected routes for the getting the individual userProfile and routes for updateProfile

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,1 +1,12 @@
+import express from "express";
+import { getuserProfile } from "../controllers/user.controller";
+import { protectedRoute } from "../middleware/auth.middleware";
 
+const router = express.Router();
+
+router.get("/profile/:username", getuserProfile);
+router.put("/profile",protectedRoute, updateProfile);
+
+// update profile => auth
+
+export default userRoutes;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 import express, { json } from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import {clerkMiddleware} from "@clerk/express"
+import userRoutes from "./routes/user.route.js";
 
 const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+app.use(clerkMiddleware());
 
 app.listen(ENV.PORT, () => console.log("Server is up and running onn port 5001"))
 
 app.get("/",(req,res) => res.send("Hello from Server"));
+
+app.use("/api/user",userRoutes)
 
 const startServer = async () => {
     try{
@@ -20,4 +29,3 @@ const startServer = async () => {
 
 startServer ();
 
-app.use(json);
